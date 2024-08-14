ddb9404c98d6034cb4ba30aad42144b46d0582df
martonchelik <84589903+martonchelik@users.noreply.github.com>
2024-08-14 16:46:18 +0300

    Coefficients added for every bet
    TODO: make them only for Aviator, JetX, Roulette and stuff

--- coefficient.js
+++ coefficient.js
@@ -1,8 +1,38 @@
 document.addEventListener('readystatechange', () => {
     if (document.readyState === 'complete') {
-        const betTable = document.getElementById('index_table_bets');
-        
-
+        setTimeout(() => changeTable(), 0)
+        function changeTable() {
+            const betTable = document.getElementById('index_table_bets');
+            const headerRow = betTable.getElementsByTagName('thead')[0].getElementsByTagName('tr')[0];
+            const newHeaderCell = document.createElement('th');
+            newHeaderCell.textContent = 'Коэфициент';
+            newHeaderCell.className = "col";
+            headerRow.insertBefore(newHeaderCell, headerRow.childNodes[16]);
+            const rows = betTable.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+            for (let i = 0; i < rows.length; i++) {
+                const newDataCell = document.createElement('td');
+                const SS = parseFloat(rows[i].childNodes[13].textContent.replace(/,/, '.'));
+                const SV = parseFloat(rows[i].childNodes[15].textContent.replace(/,/, '.'));
+                const coefficient = (SV/SS).toFixed(2)
+                const coefficientSpan = document.createElement('span');
+                coefficientSpan.className = 'status_tag';
+                coefficientSpan.style.color = 'black';
+                coefficientSpan.style.fontSize = '1em';
+                coefficientSpan.textContent = coefficient;
+                if (coefficient == 0.00){
+                    console.log();
+                } else if (coefficient < 2){
+                    coefficientSpan.classList.add('red');
+                } else if (coefficient > 3){
+                    coefficientSpan.classList.add('green');
+                } else{
+                    coefficientSpan.classList.add('warn');
+                }
+                newDataCell.appendChild(coefficientSpan)
+                newDataCell.className = "col";
+                rows[i].insertBefore(newDataCell, rows[i].childNodes[16]);
+            }
+        }
 
     }
 });
--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
-    // Function to save checkbox states
     function saveCheckboxStates() {
         const checkboxStates = {};
 
@@ -9,18 +8,15 @@ document.addEventListener('DOMContentLoaded', () => {
             checkboxStates[checkbox.id] = checkbox.checked;
         });
 
-        // Save the checkbox states to chrome.storage.local
         chrome.storage.local.set({ checkboxStates }, () => {
             console.log('Checkbox states saved:', checkboxStates);
         });
     }
 
-    // Add event listeners to each checkbox
     checkboxes.forEach((checkbox) => {
         checkbox.addEventListener('change', saveCheckboxStates);
     });
 
-    // Initial load: get the current checkbox states from chrome.storage.local
     chrome.storage.local.get('checkboxStates', (result) => {
         if (result.checkboxStates) {
             Object.keys(result.checkboxStates).forEach((id) => {
